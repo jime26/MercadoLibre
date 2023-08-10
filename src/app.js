afc16dd6b8a0e72025a17a46040808c525c1a4fd
jime026 <coca.jimena3@gmail.com>
2023-08-10 01:17:11 -0300

    views 2.0

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const path = require('path');
+const path = require ("path");
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log("Servidor corriendo en el puerto 3001"));
+const port =process.env.PORT || 3001;
+app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`));
 
-app.get("/", (req,res) =>{
-    res.sendFile(path.join(__dirname,"./views/home.html"));
-});
 
-app.get("/register", (req,res) => {
-    res.sendFile(path.join(__dirname,"./views/register.html"));
-});
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get("/registro", (req, res)=> {
+    res.sendFile(path.join(__dirname, "./views/registro.html"))
+})
+
+app.get("/login", (req, res)=> {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+})
